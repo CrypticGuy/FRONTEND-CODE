bac9b0420f2c23f00eca9de0743c9026d05b4949
digitalBeing <alohanvasu@gmail.com>
2018-05-15 01:44:13 +0530

    Corrected the timer

--- trialsite/website.js
+++ trialsite/website.js
@@ -15,16 +15,23 @@ $(document).ready(function(){
 var h= window.innerHeight;
 var i2= $('.img2').offset();
 var ar="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,  consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
-
+let timer = null;
 var i=0;
 var ne="";
 function typewriter()
-{if (i<ar.length)
-{   ne=ne+ar.charAt(i)
-    $('.descript').text(ne);
-    i++;
-    setTimeout(typewriter,50);
-}}
+{
+    if (i<ar.length)
+    {   
+        ne=ne+ar.charAt(i)
+        $('.descript').text(ne);
+        i++;
+        timer = setTimeout(typewriter,50);
+    }
+    else {
+        clearTimeout(timer)
+    }
+}
+let typewriterCalled = false;
 $(window).on('scroll',check_if_in_view);
 $(window).trigger('scroll');
 function check_if_in_view()
@@ -43,7 +50,8 @@ function check_if_in_view()
     if(scroll+h>i2.top+70)
     {
         $('.img2').addClass('animate');
-        typewriter();
+        if (typewriterCalled === false) typewriter();
+        typewriterCalled = true;
     }
     if (scroll+h>$('.divider').offset().top)
     {
